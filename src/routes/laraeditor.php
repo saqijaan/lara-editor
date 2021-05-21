60212dca4f1b4a73f901180dc5ce25ed93959be7
Muhammad Saqib Ali <saqib.topdot@gmail.com>
2021-05-21 11:19:25 +0500

    routes issue fixed

--- src/routes/laraeditor.php
+++ src/routes/laraeditor.php
@@ -5,12 +5,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::prefix('laraeditor')->name('laraeditor.')->namespace('LaraEditor\App\Http\Controllers')->group(function(){
 	Route::post('editor/asset/store', 'AssetController@store')->name('editor.asset.store');
-	
-	Route::get('editor/{model}/{editable}', 'EditorController@editor')->name('editor.model.editor');
-	Route::post('editor/{model}/{editable}', 'EditorController@store')->name('editor.model.store');
-	
-	Route::get('editor/{model}/{editable}/templates', 'EditorController@templates')->name('editor.model.templates');
-	Route::get('editor/templates', 'EditorController@templates')->name('editor.templates');
 
 	Route::get('media/{media}', 'MediaController@show')->name('media.show');
 	Route::post('media', ['MediaController@store'])->name('media.store');
