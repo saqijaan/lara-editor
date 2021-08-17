d0aa5d5df24be8ad21dbccb399d93591c8782494
saqijaan <45781603+saqijaan@users.noreply.github.com>
2021-08-17 14:42:59 +0500

    included toast js css

--- src/resources/views/editor.blade.php
+++ src/resources/views/editor.blade.php
@@ -7,7 +7,7 @@
     <meta name="csrf-token" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Edit Item</title>
-
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.css">
     <style>
         * {
             margin: 0;
